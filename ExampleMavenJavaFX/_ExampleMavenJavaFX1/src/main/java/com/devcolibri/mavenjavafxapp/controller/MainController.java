2c3d77044a5891c71091879393f352871f21e688
Roman Strelchenko <xlsr@cis-kiev.com>
2017-04-10 17:52:21 +0300

    New

---
+++ ExampleMavenJavaFX/_ExampleMavenJavaFX1/src/main/java/com/devcolibri/mavenjavafxapp/controller/MainController.java
@@ -0,0 +1,19 @@
+package com.devcolibri.mavenjavafxapp.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+public class MainController {
+    @FXML
+    private Button btn;
+
+    @FXML
+    private TextArea txtArea;
+
+    @FXML
+    public void onBtnClick(){
+
+    }
+
+}
---
+++ ExampleMavenJavaFX/src/main/java/com/devcolibri/mavenjavafxapp/controller/MainController.java
@@ -0,0 +1,24 @@
+package com.devcolibri.mavenjavafxapp.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import org.apache.log4j.Logger;
+
+public class MainController {
+
+    final static Logger logger = Logger.getLogger(MainController.class);
+
+    @FXML
+    private Button btn;
+
+    @FXML
+    private TextArea area;
+
+    @FXML
+    public void onBtnClick(){
+        area.appendText("test");
+        logger.info("test");
+    }
+
+}
