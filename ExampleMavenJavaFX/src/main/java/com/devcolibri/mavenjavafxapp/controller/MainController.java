dde9c9e7432e5e260ee029159f8a516238b44288
Roman Strelchenko <xlsr@cis-kiev.com>
2017-04-11 18:33:38 +0300

    новый функцеонал

--- ExampleMavenJavaFX/src/main/java/com/devcolibri/mavenjavafxapp/controller/MainController.java
+++ ExampleMavenJavaFX/src/main/java/com/devcolibri/mavenjavafxapp/controller/MainController.java
@@ -17,7 +17,7 @@ public class MainController {
 
     @FXML
     public void onBtnClick(){
-        area.appendText("Hello");
+        area.appendText("Hello World");
         logger.info("Hello");
     }
 
